db2a8cbab2153494ca4d872b03670d2884ff385c
Antonis Kalou <kalouantonis@protonmail.com>
2023-08-07 13:18:20 +0000

    Change version to 0.1.0

--- lib/DBIx/Class/Storage/DBI/MariaDB.pm
+++ lib/DBIx/Class/Storage/DBI/MariaDB.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use base qw/DBIx::Class::Storage::DBI/;
 
-our $VERSION = '0.000001';
+our $VERSION = '0.1.0';
 
 __PACKAGE__->sql_maker_class('DBIx::Class::SQLMaker::MySQL');
 __PACKAGE__->sql_limit_dialect('LimitXY');
