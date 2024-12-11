Siemplexus/DBIx-Class-Storage-DBI-MariaDB

commit 1cd087250dff900d12bed585c7670ca0f963d7f6

Use correct bind type for BLOB fields

lib/DBIx/Class/Storage/DBI/MariaDB.pm
@@ -3,6 +3,7 @@ package DBIx::Class::Storage::DBI::MariaDB;
 use strict;
 use warnings;
 
+use DBI;
 use base qw/DBIx::Class::Storage::DBI/;
 
 our $VERSION = '0.1.0';
@@ -171,6 +172,13 @@ sub lag_behind_master {
     return
       shift->_get_dbh->selectrow_hashref('show slave status')
       ->{Seconds_Behind_Master};
+}
+
+sub bind_attribute_by_data_type {
+    if ( $_[1] = ~ /^(?:tiny|medium|long)blob$/i ) {
+        return DBI::SQL_BINARY;
+    }
+    return;
 }
 
 1;

Makefile.PL
@@ -24,6 +24,7 @@ WriteMakefile(
     PREREQ_PM    => {
         'DBD::MariaDB' => '>= 1.00',
         'DBIx::Class'  => '>= 0.082820',
+        'DBI'          => 0,
     },
     TEST_REQUIRES => {
         'Test::More'      => 0,

commit f0f5f25775baa6ed8879f02724e90c01092fdcda

Don't apply SQL_BINARY when we should not
Fixes typo introduced in 1cd087250dff900d12bed585c7670ca0f963d7f6.

lib/DBIx/Class/Storage/DBI/MariaDB.pm
@@ -175,7 +175,7 @@ sub lag_behind_master {
 }
 
 sub bind_attribute_by_data_type {
-    if ( $_[1] = ~ /^(?:tiny|medium|long)blob$/i ) {
+    if ( $_[1] =~ /^(?:tiny|medium|long)blob$/i ) {
         return DBI::SQL_BINARY;
     }
     return;